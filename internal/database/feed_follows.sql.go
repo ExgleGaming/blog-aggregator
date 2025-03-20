5b22b5e17d019ee8f1fbc33ad72b2ba5de2a1eff
ExgleGaming <167655468+ExgleGaming@users.noreply.github.com>
2025-03-20 14:40:48 -0400

    more updates and final tests

--- internal/database/feed_follows.sql.go
+++ internal/database/feed_follows.sql.go
@@ -66,6 +66,29 @@ func (q *Queries) CreateFeedFollow(ctx context.Context, arg CreateFeedFollowPara
 	return i, err
 }
 
+const deleteFeedFollow = `-- name: DeleteFeedFollow :exec
+
+DELETE FROM feed_follows
+WHERE id IN (
+    SELECT feed_follows.id
+    FROM feed_follows
+             INNER JOIN users ON feed_follows.user_id = users.id
+             INNER JOIN feeds ON feed_follows.feed_id = feeds.id
+    WHERE users.name = $1
+      AND feeds.url = $2
+)
+`
+
+type DeleteFeedFollowParams struct {
+	Name string
+	Url  string
+}
+
+func (q *Queries) DeleteFeedFollow(ctx context.Context, arg DeleteFeedFollowParams) error {
+	_, err := q.db.ExecContext(ctx, deleteFeedFollow, arg.Name, arg.Url)
+	return err
+}
+
 const getFeedFollowsForUser = `-- name: GetFeedFollowsForUser :many
 
 SELECT feed_follows.id, feed_follows.created_at, feed_follows.updated_at, feed_follows.user_id, feed_follows.feed_id, feeds.name AS feed_name, users.name AS user_name
